cf6c5f07d5aad7de4109c3883b5a39c0c37f16ea
arfyudika <62383431+arfyudika@users.noreply.github.com>
2021-10-15 19:35:00 +0700

    Add files via upload

--- dha.js
+++ dha.js
@@ -4253,13 +4253,21 @@ break
               dha.sendMessage(from, '*Pertanyaan :* '+kapankah+'\n*Jawaban :* '+ koh, text, { quoted: mek })
               break
           case 'bucin':
-                    get_result = await fetchJson(`https://api.lolhuman.xyz/api/random/bucin?apikey=${setting.lolkey}`)
-                    reply(get_result.result)
-                    break
+              get_result = await fetchJson(`https://api.lolhuman.xyz/api/random/bucin?apikey=${setting.lolkey}`), {method: 'get'})
+              kata = anu.result
+              buttons = [{buttonId: `${prefix}bucin`,buttonText:{displayText: '️⏩ Next'},type:1},{buttonId: `${prefix}command`,buttonText:{displayText: 'Back Menu'},type:1}]
+              buttonsMessage = { contentText: `${kata}`, footerText: `Created By ${nmbot}`, buttons: buttons, headerType: 1 }
+              prep = await dha.prepareMessageFromContent(from,{buttonsMessage},{contextInfo: { mentionedJid: [sender]},quoted:mek})
+              dha.relayWAMessage(prep)
+              break
           case 'randomnama':
-                    anu = await fetchJson(`https://api.lolhuman.xyz/api/random/nama?apikey=${setting.lolkey}`)
-                    reply(anu.result)
-                    break
+              anu = await fetchJson(`https://api.lolhuman.xyz/api/random/nama?apikey=${setting.lolkey}`), {method: 'get'})
+		      kata = anu.result
+              buttons = [{buttonId: `${prefix}randomnama`,buttonText:{displayText: '️⏩ Next'},type:1},{buttonId: `${prefix}command`,buttonText:{displayText: 'Back Menu'},type:1}]
+              buttonsMessage = { contentText: `${kata}`, footerText: `Created By ${nmbot}`, buttons: buttons, headerType: 1 }
+              prep = await dha.prepareMessageFromContent(from,{buttonsMessage},{contextInfo: { mentionedJid: [sender]},quoted:mek})
+              dha.relayWAMessage(prep)
+              break
        case 'truth':
               const trut =['Pernah suka sama siapa aja? berapa lama?','Kalau boleh atau kalau mau, di gc/luar gc siapa yang akan kamu jadikan sahabat?(boleh beda/sma jenis)','apa ketakutan terbesar kamu?','pernah suka sama orang dan merasa orang itu suka sama kamu juga?','Siapa nama mantan pacar teman mu yang pernah kamu sukai diam diam?','pernah gak nyuri uang nyokap atau bokap? Alesanya?','hal yang bikin seneng pas lu lagi sedih apa','pernah cinta bertepuk sebelah tangan? kalo pernah sama siapa? rasanya gimana brou?','pernah jadi selingkuhan orang?','hal yang paling ditakutin','siapa orang yang paling berpengaruh kepada kehidupanmu','hal membanggakan apa yang kamu dapatkan di tahun ini','siapa orang yang bisa membuatmu sange','siapa orang yang pernah buatmu sange','(bgi yg muslim) pernah ga solat seharian?','Siapa yang paling mendekati tipe pasangan idealmu di sini','suka mabar(main bareng)sama siapa?','pernah nolak orang? alasannya kenapa?','Sebutkan kejadian yang bikin kamu sakit hati yang masih di inget','pencapaian yang udah didapet apa aja ditahun ini?','kebiasaan terburuk lo pas di sekolah apa?']
               const ttrth = trut[Math.floor(Math.random() * trut.length)]
@@ -4353,7 +4361,7 @@ break
 					unb = banned.indexOf(ya)
 					ban.splice(unb, 1)
 					fs.writeFileSync('./src/banned.json', JSON.stringify(banned))
-					reply(`Nomor wa.me/${ya} telah di unban!`)
+					reply(`Nomor ${ya} telah di unban!`)
           break
         case 'getpp':
                if (mek.message.extendedTextMessage === null || mek.message.extendedTextMessage === undefined) {
